65c28cc4d6c3e07c90e2386321ba9cc5070e821d
Letícia Brito <leticiabdourado@gmail.com>
2023-03-15 14:51:18 -0300

    plugin

--- js/script.js
+++ js/script.js
@@ -28,4 +28,49 @@ function ativarProduto(parametro) {
 }
 parametros.forEach(ativarProduto);
 
+//perguntas frequentes
 
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+    const pergunta = event.currentTarget
+    const controls = pergunta.getAttribute('aria-controls');
+    const resposta = document.getElementById(controls);
+
+    resposta.classList.toggle('ativa');
+    const ativa = resposta.classList.contains('ativa')
+    pergunta.setAttribute('aria-expanded', ativa);
+
+    console.log(resposta);
+}
+
+function eventosPerguntas(pergunta) {
+   pergunta.addEventListener('click', ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas)
+
+console.log(perguntas);
+
+// galeria de bicicletas 
+const galeria = document.querySelectorAll('.modelo-imagens img');
+const galeriaContainer = document.querySelector('.modelo-imagens');
+
+
+function trocarImagem(event) {
+    const img = event.currentTarget;
+    const media = matchMedia('(min-width: 1000px)');
+    console.log(media)
+    if(media) {
+    galeriaContainer.prepend(img)}
+}
+
+function eventGaleria(img) {
+img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach (eventGaleria);
+
+//animação
+if(window.SimpleAnime) {
+new SimpleAnime();}
